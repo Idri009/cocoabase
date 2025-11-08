0746d96bc64ca11538242c18358b89e804557318
seyi.idowu <shezi357412@icloud.com>
2025-11-09 00:15:29 +0100

    feat: add plantation status timeline with wallet connect integration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    const externals = config.externals ?? [];
+    if (Array.isArray(externals)) {
+      externals.push("pino-pretty", "lokijs", "encoding");
+      config.externals = externals;
+    } else {
+      config.externals = [
+        externals,
+        "pino-pretty",
+        "lokijs",
+        "encoding",
+      ];
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;

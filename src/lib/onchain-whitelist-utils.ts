bec92948e0dc07d86a6335643d5018812166d03a
seyi.idowu <shezi357412@icloud.com>
2025-11-10 23:12:52 +0100

    feat: add whitelist add, remove and check functions

--- src/lib/onchain-whitelist-utils.ts
+++ src/lib/onchain-whitelist-utils.ts
@@ -16,3 +16,34 @@ export function createWhitelist(name: string): Whitelist {
   };
 }
 
+export function addToWhitelist(
+  whitelist: Whitelist,
+  address: Address
+): Whitelist {
+  const newAddresses = new Set(whitelist.addresses);
+  newAddresses.add(address);
+  return {
+    ...whitelist,
+    addresses: newAddresses,
+  };
+}
+
+export function removeFromWhitelist(
+  whitelist: Whitelist,
+  address: Address
+): Whitelist {
+  const newAddresses = new Set(whitelist.addresses);
+  newAddresses.delete(address);
+  return {
+    ...whitelist,
+    addresses: newAddresses,
+  };
+}
+
+export function isWhitelisted(
+  whitelist: Whitelist,
+  address: Address
+): boolean {
+  if (!whitelist.enabled) return true;
+  return whitelist.addresses.has(address);
+}

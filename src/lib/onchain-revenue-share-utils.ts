a4b2f33d7bbdb1b5eeb9f394cfe79f0b268dfac5
seyi.idowu <shezi357412@icloud.com>
2025-11-10 21:56:43 +0100

    feat: add revenue share utilities

--- src/lib/onchain-revenue-share-utils.ts
+++ src/lib/onchain-revenue-share-utils.ts
@@ -5,51 +5,18 @@ export interface RevenueShare {
   token: Address;
   totalRevenue: bigint;
   shareholders: Map<Address, bigint>;
-  sharePercentages: Map<Address, number>;
+  distributionPeriod: bigint;
 }
 
 export function createRevenueShare(
-  token: Address
+  token: Address,
+  distributionPeriod: bigint
 ): RevenueShare {
   return {
     id: BigInt(0),
     token,
     totalRevenue: BigInt(0),
     shareholders: new Map(),
-    sharePercentages: new Map(),
-  };
-}
-
-export function addRevenue(
-  revenueShare: RevenueShare,
-  amount: bigint
-): RevenueShare {
-  return {
-    ...revenueShare,
-    totalRevenue: revenueShare.totalRevenue + amount,
-  };
-}
-
-export function distributeRevenue(
-  revenueShare: RevenueShare,
-  shareholder: Address
-): bigint {
-  const percentage = revenueShare.sharePercentages.get(shareholder) || 0;
-  return (revenueShare.totalRevenue * BigInt(Math.floor(percentage * 100))) / BigInt(10000);
-}
-
-export function addShareholder(
-  revenueShare: RevenueShare,
-  shareholder: Address,
-  percentage: number
-): RevenueShare {
-  const newShareholders = new Map(revenueShare.shareholders);
-  const newPercentages = new Map(revenueShare.sharePercentages);
-  newShareholders.set(shareholder, BigInt(0));
-  newPercentages.set(shareholder, percentage);
-  return {
-    ...revenueShare,
-    shareholders: newShareholders,
-    sharePercentages: newPercentages,
+    distributionPeriod,
   };
 }

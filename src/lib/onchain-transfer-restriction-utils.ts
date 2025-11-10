63382549905c5ea6e33849d7e8e2470cb3b2ba11
seyi.idowu <shezi357412@icloud.com>
2025-11-10 22:02:03 +0100

    feat: add address restriction and transfer validation

--- src/lib/onchain-transfer-restriction-utils.ts
+++ src/lib/onchain-transfer-restriction-utils.ts
@@ -24,3 +24,44 @@ export function createTransferRestriction(
   };
 }
 
+export function restrictAddress(
+  restriction: TransferRestriction,
+  address: Address
+): TransferRestriction {
+  const newRestricted = new Set(restriction.restricted);
+  newRestricted.add(address);
+  return {
+    ...restriction,
+    restricted: newRestricted,
+  };
+}
+
+export function whitelistAddress(
+  restriction: TransferRestriction,
+  address: Address
+): TransferRestriction {
+  const newWhitelist = new Set(restriction.whitelist);
+  newWhitelist.add(address);
+  return {
+    ...restriction,
+    whitelist: newWhitelist,
+  };
+}
+
+export function canTransfer(
+  restriction: TransferRestriction,
+  from: Address,
+  to: Address,
+  amount: bigint
+): boolean {
+  if (restriction.restricted.has(from) || restriction.restricted.has(to)) {
+    return false;
+  }
+  if (restriction.whitelist.has(from) || restriction.whitelist.has(to)) {
+    return true;
+  }
+  if (amount < restriction.minAmount || amount > restriction.maxAmount) {
+    return false;
+  }
+  return true;
+}
